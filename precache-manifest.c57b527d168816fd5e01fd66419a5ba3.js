35b634ab12526628cbe29f23d6c9ca7725cd13ba
Priyanshu Rathore <prathore.me@gmail.com>
2020-08-24 12:34:36 +0530

    Updates

---
+++ precache-manifest.c57b527d168816fd5e01fd66419a5ba3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f82444b0c93b7887811d27bf9cbc4ece",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "0e994529873b4d596ad5",
+    "url": "/react-todo-app/static/css/main.b99b6c57.chunk.css"
+  },
+  {
+    "revision": "401f3223b0e322297741",
+    "url": "/react-todo-app/static/js/2.1919871f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.1919871f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e994529873b4d596ad5",
+    "url": "/react-todo-app/static/js/main.f5e5ca66.chunk.js"
+  },
+  {
+    "revision": "7f607c3e693deaba8e3a",
+    "url": "/react-todo-app/static/js/runtime-main.93dbfc51.js"
+  }
+]);
